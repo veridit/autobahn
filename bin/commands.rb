4f5fef66bb4e1eddfc3dcbdc1a7d9361551aba5d
Matias Hermanrud Fjeld <matias@veridit.no>
2010-04-06 11:31:44 +0200

    Added command "generate template"

--- bin/commands.rb
+++ bin/commands.rb
@@ -5,6 +5,8 @@ program :name, 'autobahn'
 program :version, File.read(File.join(File.dirname(__FILE__), '..', 'VERSION')).chomp
 program :description, 'Enterprise Ruby on Rails'
 
+autobahn_repo = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+
 command :init do |command|
   command.syntax = "init [options] <directory>"
   command.description = "Initialize an autobahn project"
@@ -47,5 +49,25 @@ command :init do |command|
 
     system 'ruby', File.join(project_path, 'vendor', 'rails', 'railties', 'bin', 'rails'), project_path, "--template=#{template_path}"
     # TODO: Store .autobahn_revision
+  end
+end
+
+command "generate template" do |command|
+  command.syntax = 'generate template <name>'
+  command.description = "Generate an autobahn upgrade template"
+  command.action do |args, options|
+    name = args.first || ask("What should the template be named? ")
+    slug = name.downcase.gsub(/[^a-z0-9]+/, '_').split('_').join('_')
+    templates_path = File.join(autobahn_repo, 'templates')
+    if existing = Dir.entries(templates_path).select{|name| name.match(/^[0-9]+_#{slug}\.rb$/)}.first
+      puts "A template with that name already exists: #{File.join(templates_path, existing)}"
+    else
+      timestamp = DateTime.now.strftime('%Y%m%d%H%M%S')
+      path = File.join(templates_path, "#{timestamp}_#{slug}.rb")
+      File.open(path, 'w') do |file|
+        file.write("# #{name}\n")
+      end
+      puts "Created #{path}"
+    end
   end
 end
