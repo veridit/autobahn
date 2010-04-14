cb474fd24c3c5b0bd6875c84641ca5a7b073f9cd
Matias Hermanrud Fjeld <matias@veridit.no>
2010-04-14 15:12:09 +0200

    Added upgrade template setting up plugins redhillonrails_core and schema_validations

---
+++ templates/20100414131855_add_model_related_plugins.rb
@@ -0,0 +1,6 @@
+# Add model-related plugins
+
+submodule 'vendor/plugins/redhillonrails_core', :commit => '85a2212ecb63d7879c5ee722f18d467fa3ade092'
+submodule 'vendor/plugins/schema_validations', :commit => '193d26cc37cd0ecb06232e8794c8e2db81db4906'
+
+git :commit => '-m "Added plugins redhillonrails_core, schema_validations"'
